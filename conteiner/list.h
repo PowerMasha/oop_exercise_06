81be25c32f9a7b9a9027eedbf5bc74946efc7d48
PowerMasha <47854731+PowerMasha@users.noreply.github.com>
2020-01-09 06:30:05 +0300

    Add files via upload

--- conteiner/list.h
+++ conteiner/list.h
@@ -1,52 +1,52 @@
-#ifndef LIST_H
-#define LIST_H
-
+#pragma once
 #include <iterator>
 #include <memory>
 #include "../square.h"
 
 namespace containers {
 
-
     template<class T, class Allocator = std::allocator<T>>
     class list {
     private:
         struct element;
-        unsigned int size = 0;
+        size_t size = 0;
     public:
         list() = default;
 
         class forward_iterator {
         public:
             using value_type = T;
-            using reference = T&;
-            using pointer = T*;
-            using difference_type = std::ptrdiff_t; //для арифметики указателей и индексации массива
-            using iterator_category = std::forward_iterator_tag;//пустой класс для идентификации прямого итератора
+            using reference = value_type& ;
+            using pointer = value_type* ;
+            using difference_type = std::ptrdiff_t;
+            using iterator_category = std::forward_iterator_tag;
             explicit forward_iterator(element* ptr);
             T& operator*();
             forward_iterator& operator++();
             forward_iterator operator++(int);
             bool operator== (const forward_iterator& other) const;
             bool operator!= (const forward_iterator& other) const;
-
         private:
             element* it_ptr;
             friend list;
-
         };
+
         forward_iterator begin();
         forward_iterator end();
+        void push_back(const T& value);
+        void push_front(const T& value);
+        T& front();
+        T& back();
         void pop_back();
         void pop_front();
-        void push_back(const T& value);
         size_t length();
+        bool empty();
         void delete_by_it(forward_iterator d_it);
         void delete_by_number(size_t N);
         void insert_by_it(forward_iterator ins_it, T& value);
         void insert_by_number(size_t N, T& value);
-        T& operator[](size_t index) ;
-        list& operator=(list&& other);
+        list& operator=(list& other);
+        T& operator[](size_t index);
     private:
         using allocator_type = typename Allocator::template rebind<element>::other;
 
@@ -64,17 +64,20 @@ namespace containers {
             }
 
         };
+
+        using unique_ptr = std::unique_ptr<element, deleter>;
         struct element {
             T value;
-            std::shared_ptr<element> next_element;
-            std::weak_ptr<element> prev_element ;
+            unique_ptr next_element = { nullptr, deleter{nullptr} };
+            element* prev_element = nullptr;
+            element(const T& value_) : value(value_) {}
             forward_iterator next();
         };
+
         allocator_type allocator_{};
-        static std::shared_ptr<element> push_impl(std::shared_ptr<element> cur);
-        static std::shared_ptr<element> pop_impl(std::shared_ptr<element> cur);
-        std::shared_ptr<element> first = nullptr;
-    };//===============================end-of-class-list======================================//
+        unique_ptr first{ nullptr, deleter{nullptr} };
+        element* tail = nullptr;
+    };
 
     template<class T, class Allocator>
     typename list<T, Allocator>::forward_iterator list<T, Allocator>::begin() {
@@ -85,31 +88,82 @@ namespace containers {
     typename list<T, Allocator>::forward_iterator list<T, Allocator>::end() {
         return forward_iterator(nullptr);
     }
-
-//=========================base-methods-of-list==========================================//
     template<class T, class Allocator>
     size_t list<T, Allocator>::length() {
         return size;
     }
+    template<class T, class Allocator>
+    bool list<T, Allocator>::empty() {
+        return length() == 0;
+    }
 
     template<class T, class Allocator>
-    std::shared_ptr<typename list<T, Allocator>::element> list<T, Allocator>::push_impl(std::shared_ptr<element> cur) {
-        if (cur -> next_element != nullptr) {
-            return push_impl(cur->next_element);
+    void list<T, Allocator>::push_back(const T& value) {
+        element* result = this->allocator_.allocate(1);
+        std::allocator_traits<allocator_type>::construct(this->allocator_, result, value);
+        if (!size) {
+            first = unique_ptr(result, deleter{ &this->allocator_ });
+            tail = first.get();
+            size++;
+            return;
         }
-        return cur;
+        if(size !=0 ) {
+            if (result->value.Area() < tail->value.Area()) {
+                std::cout << "Area is too low"<< std::endl;
+                return;
+            }
+        }
+        element* temp =  tail;
+        tail->next_element = unique_ptr(result, deleter{ &this->allocator_ });
+        tail = temp->next_element.get();
+      
+        tail->prev_element = temp;
+
+        size++;
+
+    }
+
+    template<class T, class Allocator>
+    void list<T, Allocator>::push_front(const T& value) {
+        element* result = this->allocator_.allocate(1);
+        std::allocator_traits<allocator_type>::construct(this->allocator_, result, value);
+       if (size !=0) {
+           if (result->value.Area() > first->value.Area()) {
+               std::cout << "Area is too big" << std::endl;
+               return;
+           }
+       }
+        unique_ptr tmp = std::move(first);
+        first = unique_ptr(result, deleter{ &this->allocator_ });
+        first->next_element = std::move(tmp);
+        if(first->next_element != nullptr) {
+            first->next_element->prev_element = first.get();
+        }
+        size++;
+        if (size == 1) {
+            tail = first.get();
+        }
+        if (size == 2) {
+            tail = first->next_element.get();
+        }
     }
 
     template<class T, class Allocator>
     void list<T, Allocator>::pop_front() {
         if (size == 0) {
-            throw std::logic_error ("stack is empty");
+            throw std::logic_error("can`t pop from empty list");
+        }
+        if (size == 1) {
+            first = nullptr;
+            tail = nullptr;
+            size--;
+        }else {
+            unique_ptr tmp = std::move(first->next_element);
+            first = std::move(tmp);
+            first->prev_element = nullptr;
+            size--;
         }
 
-        first = first->next_element;
-        if(first){
-            first->prev_element.reset();}
-        size--;
     }
 
     template<class T, class Allocator>
@@ -117,126 +171,123 @@ namespace containers {
         if (size == 0) {
             throw std::logic_error("can`t pop from empty list");
         }
-        first = pop_impl(first);
-        size--;
+        if (tail->prev_element){
+            element* tmp = tail->prev_element;
+            tail->prev_element->next_element = nullptr;
+            tail = tmp;
+            size--;
+        }
+        else{
+            first = nullptr;
+            tail = nullptr;
+            size--;
+        }
+
     }
 
 
-    template<class T, class Allocator >
-    std::shared_ptr<typename list<T, Allocator>::element> list<T, Allocator>::pop_impl(std::shared_ptr<element> cur) {
-        if (cur->next_element != nullptr) {
-            cur->next_element = pop_impl(cur->next_element);
-            return cur;
+    template<class T, class Allocator>
+    T& list<T, Allocator>::front() {
+        if (size == 0) {
+            throw std::logic_error("list is empty");
         }
-        return nullptr;
+        return first->value;
     }
 
     template<class T, class Allocator>
-    void list<T, Allocator>::push_back(const T& value) {
-        element* tmp = this->allocator_.allocate(1);
-        std::allocator_traits<allocator_type>::construct(this->allocator_, tmp, value);
-        if (first == nullptr){
-            first = std::unique_ptr<element, deleter> (tmp, deleter{&this->allocator_});
-        }else{
-            std::swap(tmp->next_element, first);
-            first = std::move(std::unique_ptr<element, deleter> (tmp, deleter{&this->allocator_}));
+    T& list<T, Allocator>::back() {
+        if (size == 0) {
+            throw std::logic_error("list is empty");
         }
-        size++;
+        forward_iterator i = this->begin();
+        while ( i.it_ptr->next() != this->end()) {
+            i++;
+        }
+        return *i;
+    }
+    template<class T, class Allocator>
+    list<T,Allocator>& list<T, Allocator>::operator=(list<T, Allocator>& other) {
+        size = other.size;
+        first = std::move(other.first);
     }
 
+    template<class T, class Allocator>
+    void list<T, Allocator>::delete_by_it(containers::list<T, Allocator>::forward_iterator d_it) {
 
-//=================================advanced-methods========================================//
-
-    template<class T, class Allocator >
-    void list<T, Allocator>::delete_by_it(containers::list<T, Allocator>::forward_iterator d_it) { //удаление по итератору
-        if (d_it.it_ptr == nullptr) {
-            throw std::logic_error("попытка доступа к несуществующему элементу");
-        }
+        forward_iterator end = this->end();
+        if (d_it == end) throw std::logic_error("out of borders");
         if (d_it == this->begin()) {
             this->pop_front();
-            size --;
             return;
         }
-        if (d_it == this->end()) {
+        if (d_it.it_ptr == tail) {
             this->pop_back();
-            size --;
             return;
+        } else {
+            d_it.it_ptr->next_element->prev_element = d_it.it_ptr->prev_element;
+            d_it.it_ptr->prev_element->next_element = std::move(d_it.it_ptr->next_element);
+            size--;
         }
-        d_it.it_ptr->next_element->prev_element = d_it.it_ptr->prev_element;
-        d_it.it_ptr->prev_element.lock()->next_element = d_it.it_ptr->next_element;
 
-
-        size--;
     }
-//удаление по номеру
+
     template<class T, class Allocator>
     void list<T, Allocator>::delete_by_number(size_t N) {
         forward_iterator it = this->begin();
-        for (size_t i = 1; i <= N; ++i) {
+        for (size_t i = 0; i < N; ++i) {
             ++it;
         }
         this->delete_by_it(it);
     }
 
-    template<class T, class Allocator >
+    template<class T, class Allocator>
     void list<T, Allocator>::insert_by_it(containers::list<T, Allocator>::forward_iterator ins_it, T& value) {
-        if (first != nullptr) {
-            if (ins_it == this->begin()) {
-                std::shared_ptr<element> tmp = std::shared_ptr<element>(new element{ value });
-                tmp->next_element = first;
-                first->prev_element = tmp;
-                first = tmp;
-                if (tmp->value.Area() > tmp->next_element->value.Area()) {
-                    throw std::logic_error("Area is too big");
-                }
-                size++;
-                return;
-            }else {
-                if (ins_it.it_ptr == nullptr) {
-                    std::shared_ptr<element> tmp = std::shared_ptr<element>(new element{value});
 
-                    tmp->prev_element = push_impl(first);
-                    push_impl(first)->next_element = std::shared_ptr<element>(tmp);
-                    if (tmp->value.Area() < tmp->prev_element.lock()->value.Area()) {
-                        throw std::logic_error("Area is too low");
-                    }
-                    size++;
-                    return;
-                } else {
-                    std::shared_ptr<element> tmp = std::shared_ptr<element>(new element{value});
-                    tmp->prev_element = ins_it.it_ptr->prev_element;
-                    tmp->next_element = ins_it.it_ptr->prev_element.lock()->next_element;
-                    ins_it.it_ptr->prev_element = tmp;
-                    tmp->prev_element.lock()->next_element = tmp;
+        if (ins_it == this->begin()) {
+           this->push_front(value);
+            return;
+        }
+        if(ins_it.it_ptr == nullptr){
+            this->push_back(value);
+            return;
+        }
+            element *tmp = this->allocator_.allocate(1);
+            std::allocator_traits<allocator_type>::construct(this->allocator_, tmp, value);
+        if (tmp->value.Area() > ins_it.it_ptr->value.Area()) {
+            std::cout << "Area is too big"<< std::endl;
+            return;
+        }
+        else if (tmp->value.Area() < ins_it.it_ptr->prev_element->value.Area()) {
+            std::cout << "Area is too low"<< std::endl;
+            return;
+        }
+            size++;
+            tmp->prev_element = ins_it.it_ptr->prev_element;
+            tmp->next_element = std::move(tmp->prev_element->next_element);
+            tmp->next_element->prev_element = tmp;
+            tmp->prev_element->next_element = unique_ptr(tmp, deleter{&this->allocator_});
 
-                    if (tmp->value.Area() > tmp->next_element->value.Area()) {
-                        throw std::logic_error("Area is too big");
-                    }
-                    if (tmp->value.Area() < tmp->prev_element.lock()->value.Area()) {
-                        throw std::logic_error("Area is too low");
-                    }
-                }
-            }
-        } else first=std::shared_ptr<element>(new element{value});
 
-        size++;
+
     }
 
     template<class T, class Allocator>
     void list<T, Allocator>::insert_by_number(size_t N, T& value) {
         forward_iterator it = this->begin();
-        for (size_t i = 0; i < N; ++i) {
-            ++it;
-        }
+        if (N >= this->length())
+            it = this->end();
+        else
+            for (size_t i = 0; i < N; ++i) {
+                ++it;
+            }
         this->insert_by_it(it, value);
     }
-//==============================iterator`s-stuff=======================================//
-    template<class T, class Allocator >
-    typename list<T, Allocator>::forward_iterator list<T, Allocator>::element::next() {
+    template<class T, class Allocator>
+    typename list<T,Allocator>::forward_iterator list<T, Allocator>::element::next() {
         return forward_iterator(this->next_element.get());
     }
 
-    template<class T, class Allocator >
+    template<class T, class Allocator>
     list<T, Allocator>::forward_iterator::forward_iterator(containers::list<T, Allocator>::element *ptr) {
         it_ptr = ptr;
     }
@@ -245,10 +296,21 @@ namespace containers {
     T& list<T, Allocator>::forward_iterator::operator*() {
         return this->it_ptr->value;
     }
+    template<class T, class Allocator>
+    T& list<T, Allocator>::operator[](size_t index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("out of list's borders");
+        }
+        forward_iterator it = this->begin();
+        for (size_t i = 0; i < index; i++) {
+            it++;
+        }
+        return *it;
+    }
 
     template<class T, class Allocator>
     typename list<T, Allocator>::forward_iterator& list<T, Allocator>::forward_iterator::operator++() {
-        if (it_ptr == nullptr) throw std::logic_error ("out of list borders");
+        if (it_ptr == nullptr) throw std::logic_error("out of list borders");
         *this = it_ptr->next();
         return *this;
     }
@@ -260,32 +322,13 @@ namespace containers {
         return old;
     }
 
-    template<class T, class Allocator >
+    template<class T, class Allocator>
     bool list<T, Allocator>::forward_iterator::operator==(const forward_iterator& other) const {
         return it_ptr == other.it_ptr;
-    }
-    template<class T, class Allocator >
-    list<T, Allocator>& list<T, Allocator>::operator=(list<T, Allocator>&& other){
-        size = other.size;
-        first = std::move(other.first);
     }
 
     template<class T, class Allocator>
     bool list<T, Allocator>::forward_iterator::operator!=(const forward_iterator& other) const {
         return it_ptr != other.it_ptr;
     }
-
-    template<class T, class Allocator>
-    T& list<T, Allocator>::operator[](size_t index)  {
-        if (index < 0 || index >= size) {
-            throw std::out_of_range("out of list's borders");
-        }
-        forward_iterator it = this->begin();
-        for (size_t i = 0; i < index; i++) {
-            it++;
-        }
-        return *it;
-    }
-
 }
-#endif //LIST_H
